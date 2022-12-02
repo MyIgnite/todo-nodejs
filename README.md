3d727a0c3cecf39ef5b5eab673a379ce545bfe92
Renan Moreira <renan.tsx@gmail.com>
2022-12-02 15:26:58 -0300

    Adicionar usuário

--- README.md
+++ README.md
@@ -9,3 +9,14 @@ Clone o projeto e execute local:</br>
 
 `yarn` </br>
 `yarn dev` </br>
+
+[x] - Should be able to create a new user
+[x] - Should not be able to create a new user when username already exists
+[] - Should be able to list all user's todos
+[] - Should be able to create a new todo
+[] - Should be able to update a todo
+[] - Should not be able to update a non existing todo
+[] - Should be able to mark a todo as done
+[] - Should not be able to mark a non existing todo as done
+[] - Should be able to delete a todo
+[] - Should not be able to delete a non existing todo
--- src/index.js
+++ src/index.js
@@ -1,21 +1,46 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find((user) => user.username === username);
+
+  if (user) {
+    request.user = user;
+    return next();
+  }
+
+  return response.status(404).json({ error: "User not found!" });
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    const newUser = {
+      id: uuidv4(),
+      name,
+      username,
+      todos: []
+    }
+
+    users.push(newUser);
+
+    return response.status(201).json(newUser);
+  }
+
+  return response.status(400).json({ error: "User exists" });
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
